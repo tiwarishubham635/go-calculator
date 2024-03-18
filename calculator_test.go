fc6b558360e126a4082cee4a67f5a0a2a4bc23a4
Shubham Tiwari <stiwari@twilio.com>
2024-03-19 00:11:57 +0530

    feat: added tests

---
+++ calculator_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/tiwarishubham635/math"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	num1 := 3
+	num2 := 4
+	sum := math.Add(num1, num2)
+	assert.Equal(t, sum, 7)
+}
+
+func TestSubtract(t *testing.T) {
+	num1 := 3
+	num2 := 5
+	diff := math.Subtract(num1, num2)
+	assert.Equal(t, diff, -2)
+}
+
+func TestMultiply(t *testing.T) {
+	num1 := 7
+	num2 := 4
+	prod := math.Multiply(num1, num2)
+	assert.Equal(t, prod, 28)
+}
+
+func TestDivisionHappyCase(t *testing.T) {
+	num1 := 14
+	num2 := 2
+	quo, err := math.Divide(num1, num2)
+	assert.Nil(t, err)
+	assert.Equal(t, quo, 7)
+}
+
+func TestDivisionSadCase(t *testing.T) {
+	num1 := 4
+	num2 := 0
+	quo, err := math.Divide(num1, num2)
+	assert.NotNil(t, err)
+	assert.Equal(t, quo, -1)
+}
