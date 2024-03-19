a427c9fe1aaa7ec7c665c9abdd8e006c07f7fd47
Shubham Tiwari <stiwari@twilio.com>
2024-03-19 14:19:47 +0530

    feat: changes for v0.1.4

--- math/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/tiwarishubham635/math
-
-go 1.21.4
--- math/output_test.go
+++ math/output_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/tiwarishubham635/math"
 	"testing"
 )
 
