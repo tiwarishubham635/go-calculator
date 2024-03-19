f7662b466ea5da209e3af453bb84d5392f7c1300
Shubham Tiwari <stiwari@twilio.com>
2024-03-19 14:33:57 +0530

    fix: made same package

--- math/output_test.go
+++ math/output_test.go
@@ -1,36 +1,35 @@
-package math_test
+package math
 
 import (
 	"github.com/stretchr/testify/assert"
-	"github.com/tiwarishubham635/go-calculator/math"
 	"testing"
 )
 
 func TestAdd(t *testing.T) {
 	num1 := 3
 	num2 := 4
-	sum := math.Add(num1, num2)
+	sum := Add(num1, num2)
 	assert.Equal(t, sum, 7)
 }
 
 func TestSubtract(t *testing.T) {
 	num1 := 3
 	num2 := 5
-	diff := math.Subtract(num1, num2)
+	diff := Subtract(num1, num2)
 	assert.Equal(t, diff, -2)
 }
 
 func TestMultiply(t *testing.T) {
 	num1 := 7
 	num2 := 4
-	prod := math.Multiply(num1, num2)
+	prod := Multiply(num1, num2)
 	assert.Equal(t, prod, 28)
 }
 
 func TestDivisionHappyCase(t *testing.T) {
 	num1 := 14
 	num2 := 2
-	quo, err := math.Divide(num1, num2)
+	quo, err := Divide(num1, num2)
 	assert.Nil(t, err)
 	assert.Equal(t, quo, 7)
 }
@@ -38,7 +37,7 @@ func TestDivisionHappyCase(t *testing.T) {
 func TestDivisionSadCase(t *testing.T) {
 	num1 := 4
 	num2 := 0
-	quo, err := math.Divide(num1, num2)
+	quo, err := Divide(num1, num2)
 	assert.NotNil(t, err)
 	assert.Equal(t, quo, -1)
 }
